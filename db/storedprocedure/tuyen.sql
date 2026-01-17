e92b297749072b193e534dbbeb17fc91a33c71b7
21120581 <thuytruc011258@gmail.com>
2026-01-17 23:13:46 +0700

    Update tuyen.sql

--- db/storedprocedure/tuyen.sql
+++ db/storedprocedure/tuyen.sql
@@ -3,7 +3,6 @@ CREATE OR ALTER PROC usp_CapNhatTuyen
 	@MaTuyen NCHAR(4),
 	@TenTuyen NVARCHAR(50),
 	@DanhSachGa dbo.TVP_DanhSachGaTrongTuyen READONLY,
-	@MaNVThucHien NCHAR(10),
 	@ThongBao NVARCHAR(200) OUT
 AS
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
@@ -23,34 +22,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 2. Kiểm tra nhân viên thực hiện tồn tại
-	IF NOT EXISTS (
-		SELECT 1
-		FROM NHAN_VIEN
-		WHERE MaNV = @MaNVThucHien
-	)
-	BEGIN
-		SET @ThongBao = N'Nhân viên không tồn tại.';
-		ROLLBACK TRAN;
-		RETURN -1011;
-	END;
-
-
-	-- 3. Kiểm tra nhân viên thực hiện có quyền quản lý tuyến này không
-	IF NOT EXISTS (
-		SELECT 1
-		FROM TUYEN
-		WHERE MaTuyen = @MaTuyen
-		  AND MaNVQL = @MaNVThucHien
-	)
-	BEGIN
-		SET @ThongBao = N'Tuyến không thuộc quyền quản lý của nhân viên này.';
-		ROLLBACK TRAN;
-		RETURN -1012;
-	END;
-
-	
-	-- 4. Kiểm tra có tồn tại chuyến chưa kết thúc nhưng đã có đơn đặt vé thuộc tuyến này không
+	-- 2. Kiểm tra có tồn tại chuyến chưa kết thúc nhưng đã có đơn đặt vé thuộc tuyến này không
 	IF EXISTS (
 		SELECT 1
 		FROM CHUYEN_TAU ct
@@ -69,7 +41,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 5. Kiểm tra tên tuyến có rỗng không
+	-- 3. Kiểm tra tên tuyến có rỗng không
 	IF LTRIM(RTRIM(@TenTuyen)) = ''
 	BEGIN
 		SET @ThongBao = N'Tên tuyến không được rỗng.';
@@ -78,7 +50,7 @@ BEGIN TRAN;
 	END;
 
 	
-	-- 6. Kiểm tra ga tồn tại
+	-- 4. Kiểm tra ga tồn tại
 	IF EXISTS (
 		SELECT 1
 		FROM @DanhSachGa ds
@@ -95,7 +67,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 7. Kiểm tra ga bị trùng
+	-- 5. Kiểm tra ga bị trùng
 	IF EXISTS (
 		SELECT MaGa
 		FROM @DanhSachGa
@@ -109,7 +81,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 8. Kiểm tra trình tự của ga có là số dương không
+	-- 6. Kiểm tra trình tự của ga có là số dương không
 	IF EXISTS (
 		SELECT 1 
 		FROM @DanhSachGa 
@@ -122,7 +94,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 9. Kiểm tra trình tự của ga bị trùng
+	-- 7. Kiểm tra trình tự của ga bị trùng
 	IF EXISTS (
 		SELECT TrinhTu
 		FROM @DanhSachGa
@@ -136,7 +108,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 10. Kiểm tra Khoảng cách giữa các ga có là số không âm không
+	-- 8. Kiểm tra khoảng cách giữa các ga có là số không âm không
 	IF EXISTS (
 		SELECT 1
 		FROM @DanhSachGa
@@ -149,7 +121,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 11. Kiểm tra ga đầu phải có thời gian di chuyển đến và khoảng cách bằng 0
+	-- 9. Kiểm tra ga đầu phải có thời gian di chuyển đến và khoảng cách bằng 0
 	-- Lấy thời gian di chuyển đến và khoảng cách
 	DECLARE @TGDiChuyenDenGaDau TIME;
 	DECLARE @KhoangCachDenGaDau DECIMAL(6,2);
@@ -177,7 +149,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 12. Cập nhật danh sách ga của tuyến
+	-- 10. Cập nhật danh sách ga của tuyến
 	-- Xóa danh sách ga cũ của tuyến
 	DELETE FROM TUYEN_GA
 	WHERE MaTuyen = @MaTuyen;
@@ -188,6 +160,7 @@ BEGIN TRAN;
 		ROLLBACK TRAN;
 		RETURN -9202;
 	END;
+
 
 	-- Thêm danh sách ga mới cho tuyến
 	INSERT INTO TUYEN_GA (
@@ -213,7 +186,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 13. Cập nhật danh sách ga của chuyến thuộc tuyến
+	-- 11. Cập nhật danh sách ga của chuyến thuộc tuyến
 	-- Lấy danh sách chuyến cần được cập nhật thuộc tuyến này (là chuyến chưa kết thúc và cũng chưa có đơn đặt vé)
 	SELECT MaChuyenTau
 	INTO #ChuyenCanCapNhat
@@ -255,7 +228,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 14. Cập nhật thông tin tuyến
+	-- 12. Cập nhật thông tin tuyến
 	UPDATE TUYEN
 	SET TenTuyen = @TenTuyen
 	WHERE MaTuyen = @MaTuyen;
@@ -277,7 +250,6 @@ GO
 /* Xóa tuyến */
 CREATE OR ALTER PROC usp_XoaTuyen
 	@MaTuyen NCHAR(4),
-	@MaNVThucHien NCHAR(10),
 	@ThongBao NVARCHAR(200) OUT
 AS
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
@@ -296,35 +268,8 @@ BEGIN TRAN;
 		RETURN -1003;
 	END;
 
-
-	-- 2. Kiểm tra nhân viên thực hiện tồn tại
-	IF NOT EXISTS (
-		SELECT 1
-		FROM NHAN_VIEN
-		WHERE MaNV = @MaNVThucHien
-	)
-	BEGIN
-		SET @ThongBao = N'Nhân viên không tồn tại.';
-		ROLLBACK TRAN;
-		RETURN -1011;
-	END;
-
-
-	-- 3. Kiểm tra nhân viên thực hiện có quyền quản lý tuyến này không
-	IF NOT EXISTS (
-		SELECT 1
-		FROM TUYEN
-		WHERE MaTuyen = @MaTuyen
-		  AND MaNVQL = @MaNVThucHien
-	)
-	BEGIN
-		SET @ThongBao = N'Tuyến không thuộc quyền quản lý của nhân viên này.';
-		ROLLBACK TRAN;
-		RETURN -1012;
-	END;
-
 	
-	-- 4. Kiểm tra có tồn tại chuyến thuộc tuyến này không
+	-- 2. Kiểm tra có tồn tại chuyến thuộc tuyến này không
 	IF EXISTS (
 		SELECT 1
 		FROM CHUYEN_TAU
@@ -337,7 +282,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 5. Xóa các liên kết đoàn tàu với tuyến
+	-- 3. Xóa các liên kết đoàn tàu với tuyến
 	DELETE FROM TUYEN_DOANTAU
 	WHERE MaTuyen = @MaTuyen;
 
@@ -349,7 +294,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 6. Xóa danh sách ga của tuyến
+	-- 4. Xóa danh sách ga của tuyến
 	DELETE FROM TUYEN_GA
 	WHERE MaTuyen = @MaTuyen;
 
@@ -361,7 +306,7 @@ BEGIN TRAN;
 	END;
 
 
-	-- 7. Xóa tuyến
+	-- 5. Xóa tuyến
 	DELETE FROM TUYEN
 	WHERE MaTuyen = @MaTuyen;
 
